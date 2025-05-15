f572fc457d419df3e942e9711c3f7a64f215edf4
One-percent-athlete <one.percent.athlete@gmail.com>
2025-05-15 11:47:01 +0900

    Update admin py

--- genba_manager_app/admin.py
+++ genba_manager_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile, Genba, Notification, DailyReport
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Genba)
+admin.site.register(Notification)
+admin.site.register(DailyReport)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
