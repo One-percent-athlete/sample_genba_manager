63c33016b5b3161776db29a3ff8f2b1335996384
One-percent-athlete <one.percent.athlete@gmail.com>
2025-05-16 14:58:29 +0900

    Handle railway database

--- genba_manager/settings.py
+++ genba_manager/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'genba_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NcWcicFQYoyYFEFdATYDMuKLqsvlbgHI',
+        'HOST': 'caboose.proxy.rlwy.net',
+        'PORT': '49240',
     }
 }
 
