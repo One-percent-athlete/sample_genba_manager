d13fdf3efb5389504a4463dff1107a90b6839011
One-percent-athlete <one.percent.athlete@gmail.com>
2025-05-16 13:49:06 +0900

    Create add report html

--- genba_manager_app/views.py
+++ genba_manager_app/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 from django.contrib import messages
